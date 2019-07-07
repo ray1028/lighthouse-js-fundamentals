6bcdfff6809b7940f167ef46942d7af26c803cd0
rayjay1028 <rayjay1028@gmail.com>
2019-07-07 02:03:14 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -11,3 +11,10 @@ if (cold) {
 
 console.log("Now you're ready to go outside!");
 
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
